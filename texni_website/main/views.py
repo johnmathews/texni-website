ae816b7ce63224a9e8a719df684ea603948f320f
JohnMathews <mthwsjc@gmail.com>
2019-08-31 14:56:25 +0200

    add about_us page with test, use reverse(), include main.urls

--- texni_website/main/views.py
+++ texni_website/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# this is what django.views.generic.TemplateView does.
+# this is an example, it is not needed.
+def homeView(request):
+    return render(request, "home.html", {})
