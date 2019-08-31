c65bab31dab1ef52a6efc1a65042a920250180a6
JohnMathews <mthwsjc@gmail.com>
2019-08-31 15:59:51 +0200

    add foms for contact_us, add tests

--- texni_website/main/views.py
+++ texni_website/main/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from main import forms
 
 # this is what django.views.generic.TemplateView does.
 # this is an example, it is not needed.
 def homeView(request):
     return render(request, "home.html", {})
+
+
+class ContactUsView(FormView):
+    template_name = "contact_form.html" # this is how it knows which template to use in urls.py
+    form_class = forms.ContactForm # this is how it knows which form to use in the template
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
